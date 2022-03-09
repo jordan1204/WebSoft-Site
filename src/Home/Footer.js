6d9730df7b099ed83d56b4aab910310f410ed216
jordan1204 <jordan@websoft.com.tw>
2022-03-09 21:23:15 +0800

    feat:首頁footer加上CopyRight
    調整內容:
    1.footer加上CopyRight，並把"公司地址"和"連絡電話"放在同一列

--- src/Home/Footer.js
+++ src/Home/Footer.js
@@ -1,14 +1,12 @@
 import { makeStyles } from '@mui/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const useStyles = makeStyles({
     Footer: {
         paddingLeft:'16px'
-    },
-    Address: {
-        marginBottom: '2em',
-        fontSize:'18px'
     },
     PhoneNumber: {
         fontSize: '1em',
@@ -19,44 +17,52 @@ const useStyles = makeStyles({
         color:'gray'
     },
     Phone: {
-        fontSize:'18px'
+        fontSize: '18px',
+    },
+    Address: {
+        fontSize: '18px',
+    },
+    CopyRight: {
+        fontSize: "12px",
+        marginTop:"15px"
     }
   });
 
 const Footer = () => { 
     const classes = useStyles();
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('md'));
     return (
         <Paper elevation={6} sx={{
             marginTop: '50px',
             paddingTop:"20px",
             paddingBottom:"30px"
         }}>
-        <Grid container component="footer" className={classes.Footer}>
-            <Grid item xs={12} md={4}>
-                
-            </Grid>
-            <Grid item xs={12} md={4}>
-                <div className={classes.Address}>
-                    <div>
-                        公司地址
-                    </div>
-                    <div className={classes.AddressText}>
-                        新竹縣竹北市光明三路62號 2F
+            <footer className={classes.Footer}>
+                <div style={matches ? { width: "60%",margin:"0 auto" } : {width:"100%"}}>
+                    <Grid container>
+                        <Grid item xs={12} md={6}>
+                            <div className={classes.Address}>
+                                公司地址
+                            </div>
+                            <div className={classes.AddressText}>
+                                新竹縣竹北市光明三路62號2F
+                            </div> 
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <div className={classes.Phone}>
+                                連絡電話
+                            </div>
+                            <div className={classes.PhoneNumber}>
+                                036681293
+                            </div> 
+                        </Grid>
+                    </Grid>
+                    <div className={classes.CopyRight}>
+                        Copyright © {new Date().getFullYear()} WebSoft. All Rights Reserved.
                     </div>
                 </div>
-                <div>
-                    <div className={classes.Phone}>
-                        連絡電話
-                    </div>
-                    <div className={classes.PhoneNumber}>
-                        036681293
-                    </div> 
-                </div>
-            </Grid>
-            <Grid item xs={12} md={4}>
-                
-            </Grid>
-            </Grid>
+        </footer>
         </Paper>
     );
 
